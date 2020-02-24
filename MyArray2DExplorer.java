9a8edb7fce6c5b0b91907a3d355f38dd18554e32
Hamza Benaggoun <ductapedude33@gmail.com>
2020-02-24 08:57:56 -0800

    finish this stuff up

--- MyArray2DExplorer.java
+++ MyArray2DExplorer.java
@@ -1,26 +1,26 @@
-/*
+
 public class MyArray2DExplorer {
 
     public static void main(String[] args) {
-        int array [][] = {  {4,1,8,5},
-                {0,2,3,4},
-                {6,6,2,2} };
+        int array[][] = {{4, 1, 8, 5},
+                {0, 2, 3, 4},
+                {6, 6, 2, 2}};
         MyArray2DExplorer exp = new MyArray2DExplorer();
 
-        System.out.println("Test evenRow: \n Row w/ odd, Expecting: false \n Actual: " + exp.evenRow(array,0));
-        System.out.println("Test evenRow: \n Row w/o odd, Expecting: true \n Actual: " + exp.evenRow(array,2));
+        System.out.println("Test evenRow: \n Row w/ odd, Expecting: false \n Actual: " + exp.evenRow(array, 0));
+        System.out.println("Test evenRow: \n Row w/o odd, Expecting: true \n Actual: " + exp.evenRow(array, 2));
 
         System.out.println();
 
-        System.out.println("Test oddColSum: \n Col w/ odd Sum, Expecting: true \n Actual: " + exp.oddColSum(array,1));
-        System.out.println("Test oddColSum: \n Col w/ even, Expecting: false \n Actual: " + exp.oddColSum(array,0));
+        System.out.println("Test oddColSum: \n Col w/ odd Sum, Expecting: true \n Actual: " + exp.oddColSum(array, 1));
+        System.out.println("Test oddColSum: \n Col w/ even, Expecting: false \n Actual: " + exp.oddColSum(array, 0));
 
         System.out.println();
 
 
         System.out.print("Test minRow: \n Expecting: {0 2 3 4} \n Actual: {");
-        int [] row = exp.minRowPractice(array);
-        for(int i = 0; i < row.length; i++){
+        int[] row = exp.minRow(array);
+        for (int i = 0; i < row.length; i++) {
             System.out.print(row[i] + " ");
         }
         System.out.print("]");
@@ -33,8 +33,8 @@ public class MyArray2DExplorer {
         System.out.println();
 
         System.out.print("Test colMax: \n Expecting: {6 6 8 5} \n Actual: {");
-        int [] colMaxs = exp.colMaxs(array);
-        for(int i = 0; i < colMaxs.length; i++){
+        int[] colMaxs = exp.colMaxs(array);
+        for (int i = 0; i < colMaxs.length; i++) {
             System.out.print(colMaxs[i] + " ");
         }
         System.out.print("}");
@@ -43,8 +43,8 @@ public class MyArray2DExplorer {
         System.out.println();
 
         System.out.print("Test allRowSum: \n Expecting: {18 9 16} \n Actual: {");
-        int [] sum = exp.allRowSums(array);
-        for(int i = 0; i < sum.length; i++){
+        int[] sum = exp.allRowSums(array);
+        for (int i = 0; i < sum.length; i++) {
             System.out.print(sum[i] + " ");
         }
         System.out.print("}");
@@ -53,8 +53,8 @@ public class MyArray2DExplorer {
         System.out.println();
 
         System.out.print("Test averageCol: \n Expecting: {3.333333 3.0 4.333333 3.6666667} \n Actual: {");
-        double [] avg = exp.averageCol(array);
-        for(int i = 0; i < avg.length; i++){
+        double[] avg = exp.averageCol(array);
+        for (int i = 0; i < avg.length; i++) {
             System.out.print(avg[i] + " ");
         }
         System.out.print("}");
@@ -73,21 +73,21 @@ public class MyArray2DExplorer {
         System.out.println(exp.biggestRow(array));
 
         System.out.println();
+    }
 
-    //WRITE YOUR METHODS BELOW
+        //WRITE YOUR METHODS BELOW
 
-    public boolean evenRow(int [][] mat, int row){
-        boolean check = true;
-        for (int i = 0 ; i < mat[row].length ; i++){
-            if (mat[row][i] % 2 != 0){
-                check = false;
+        public boolean evenRow ( int[][] mat, int row){
+            boolean check = true;
+            for (int i = 0; i < mat[row].length; i++) {
+                if (mat[row][i] % 2 != 0) {
+                    check = false;
+                }
             }
+            return check;
+
+            //returns true if the sum of the numbers the given col is odd.
         }
-        return check;
-
-    //returns true if the sum of the numbers the given col is odd.
-}
-
 
     public boolean oddColSum(int [][] nums , int col){
         int sum = 0;
@@ -117,8 +117,17 @@ public class MyArray2DExplorer {
         }
         return null;
     }
+    public int minRowAssignment(int[][] array , int row){
+        int smallest = 10;
+        for (int i = 0; i < array[row].length ;i++ ){
+            if (array[row][i] <= smallest){
+                smallest = array[row][i];
+            }
+        }
+        return smallest;
+    }
 
-    public static int[] colMax(int[][] matrix){ //FIX THIS METHOD
+    public static int[] colMaxs(int[][] matrix){ //FIX THIS METHOD
         int[] colsMax = new int[matrix[0].length];
         for (int i = 0; i < matrix.length ; i++){
             int max = matrix[0][i];
@@ -190,16 +199,9 @@ public class MyArray2DExplorer {
         return b;
 
     }
-
-
-
-
-
-
-
-
 }
 
+        /*
         int array [][] = {  {4,1,8,5},
                 {0,2,3,4},
                 {6,6,2,2} };
